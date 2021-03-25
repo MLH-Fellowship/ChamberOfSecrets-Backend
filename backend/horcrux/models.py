dd6f7e547b63665144419a4a1e45be10d6ac74cc
Aman Sharma <amansharma2910@gmail.com>
2021-03-26 00:39:28 +0530

    Restructure file DB

--- backend/horcrux/models.py
+++ backend/horcrux/models.py
@@ -8,17 +8,13 @@ from authenticate.models import CustomUser
 
 
 class FileData(models.Model):
-    user_id = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(to=CustomUser, to_field="user_id", on_delete=models.CASCADE)
     file_id = models.AutoField(primary_key=True)
     file_name = models.CharField(max_length=1024)
+    split_1 = models.URLField()
+    split_2 = models.URLField()
+    split_3 = models.URLField()
 
     class Meta:
         ordering = ['file_id']
         unique_together = [['user_id', 'file_id']]
-
-
-class FileParts(models.Model):
-    file_id = models.ForeignKey(to=FileData, on_delete=models.CASCADE)
-    split_1 = models.URLField()
-    split_2 = models.URLField()
-    split_3 = models.URLField()
