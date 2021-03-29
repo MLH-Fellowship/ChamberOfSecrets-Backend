707fd3bd1351f2b89eb01cd272cf3a7a89d0374b
Aman Sharma <amansharma2910@gmail.com>
2021-03-29 13:15:05 +0530

    Comment out UserInfo serealization

--- backend/authenticate/views.py
+++ backend/authenticate/views.py
@@ -40,10 +40,12 @@ class UserList(APIView):
     def post(self, request, format=None):
         serializer_user = UserSerializerWithToken(data=request.data)  # seralizer for User model 
         serializer_userinfo = UserInfoSerializer(data=request.data)  # serializer for UserInfo model
+        
         if serializer_user.is_valid():
             serializer_user.save()  # saves the user model
-            if serializer_userinfo.is_valid():
-                user = User.objects.get(username=request.data['username'])
-                serializer_userinfo.save(username=user)  # save userinfo model
-                return Response(serializer_user.data, status=status.HTTP_201_CREATED)  # returns response with JWT token
+            return Response(serializer_user.data, status=status.HTTP_201_CREATED)  # TODO: remove this line and uncomment the bottom part later
+            # if serializer_userinfo.is_valid():
+            #     user = User.objects.get(username=request.data['username'])
+            #     serializer_userinfo.save(username=user)  # save userinfo model
+            #     return Response(serializer_user.data, status=status.HTTP_201_CREATED)  # returns response with JWT token
         return Response(serializer_user.errors, status=status.HTTP_400_BAD_REQUEST)  # returns response error code
