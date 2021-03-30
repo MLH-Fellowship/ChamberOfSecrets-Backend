9eda78e3b9907ebb706331926f295ad9bc617247
Aman Sharma <amansharma2910@gmail.com>
2021-03-30 14:50:18 +0530

    Add File upload and delete endpoint

--- backend/horcrux/models.py
+++ backend/horcrux/models.py
@@ -6,7 +6,7 @@ sys.path.append("..") # Adds higher directory to python modules path.
 from authenticate.models import UserInfo
 # Create your models here.
 
-
+# Django model to store horcrux info
 class FileData(models.Model):
     user_id = models.ForeignKey(UserInfo, to_field="user_id", on_delete=models.CASCADE)
     file_id = models.AutoField(primary_key=True)
@@ -18,3 +18,7 @@ class FileData(models.Model):
     class Meta:
         ordering = ['file_id']
         unique_together = [['user_id', 'file_id']]
+
+# django model for temp file storage
+class FileUpload(models.Model):
+    file_uploaded = models.FileField(upload_to="files/")
